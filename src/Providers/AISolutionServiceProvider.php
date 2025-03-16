fcc971cef0d26b956d9a2177dfc9e927f8422ab9
nnovosad <nnovosad19@gmail.com>
2025-03-17 00:19:57 +0300

    Remove config

--- src/Providers/AISolutionServiceProvider.php
+++ src/Providers/AISolutionServiceProvider.php
@@ -14,17 +14,10 @@ class AISolutionServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->publishes([
-            __DIR__ . '/../config/config.php' => config_path('ai-solution.php'),
-        ], 'config');
     }
 
     public function register(): void
     {
-        $this->mergeConfigFrom(
-            __DIR__ . '/../config/config.php', 'ai-solution'
-        );
-
         $this->app->bind(ClientInterface::class, Client::class);
 
         app(SolutionProviderRepository::class)
