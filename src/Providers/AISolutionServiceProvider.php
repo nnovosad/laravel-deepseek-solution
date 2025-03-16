9c850e026b262243e2ae305bafb520b03165a65d
nnovosad <nnovosad19@gmail.com>
2025-03-17 00:24:25 +0300

    Add interface SolutionProviderRepository for realizing

--- src/Providers/AISolutionServiceProvider.php
+++ src/Providers/AISolutionServiceProvider.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace NNovosad19\AISolution\Providers;
 
-use NNovosad19\AISolition\Providers\AISolutionProvider;
+use NNovosad19\AISolution\Providers\AISolutionProvider;
 use GuzzleHttp\Client;
 use Illuminate\Support\ServiceProvider;
 use Psr\Http\Client\ClientInterface;
 use Spatie\ErrorSolutions\Contracts\SolutionProviderRepository;
+use Spatie\ErrorSolutions\SolutionProviders\SolutionProviderRepository as SpatieSolutionProviderRepository;
 
 class AISolutionServiceProvider extends ServiceProvider
 {
@@ -18,9 +19,17 @@ class AISolutionServiceProvider extends ServiceProvider
 
     public function register(): void
     {
+        // Привязка интерфейса HTTP-клиента
         $this->app->bind(ClientInterface::class, Client::class);
 
-        app(SolutionProviderRepository::class)
+        // Привязка интерфейса SolutionProviderRepository к реализации
+        $this->app->bind(
+            SolutionProviderRepository::class,
+            SpatieSolutionProviderRepository::class
+        );
+
+        // Регистрация провайдера решений
+        $this->app->make(SolutionProviderRepository::class)
             ->registerSolutionProvider(AISolutionProvider::class);
     }
 }
