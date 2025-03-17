8bd6daac5cee502e5b9d91bb59c99e8470208e25
nnovosad <nnovosad19@gmail.com>
2025-03-17 11:00:30 +0300

    Fix binding for SolutionProviderRepository

--- src/Providers/AISolutionServiceProvider.php
+++ src/Providers/AISolutionServiceProvider.php
@@ -8,8 +8,8 @@ use NNovosad19\AISolution\Providers\AISolutionProvider;
 use GuzzleHttp\Client;
 use Illuminate\Support\ServiceProvider;
 use Psr\Http\Client\ClientInterface;
-use Spatie\ErrorSolutions\Contracts\SolutionProviderRepository;
-use Spatie\ErrorSolutions\SolutionProviders\SolutionProviderRepository as SpatieSolutionProviderRepository;
+use Spatie\ErrorSolutions\SolutionProviderRepository;
+use Spatie\ErrorSolutions\Contracts\SolutionProviderRepository as SolutionProviderRepositoryContract;
 
 class AISolutionServiceProvider extends ServiceProvider
 {
@@ -19,16 +19,13 @@ class AISolutionServiceProvider extends ServiceProvider
 
     public function register(): void
     {
-        // Привязка интерфейса HTTP-клиента
         $this->app->bind(ClientInterface::class, Client::class);
 
-        // Привязка интерфейса SolutionProviderRepository к реализации
         $this->app->bind(
-            SolutionProviderRepository::class,
-            SpatieSolutionProviderRepository::class
+            SolutionProviderRepositoryContract::class,
+            SolutionProviderRepository::class
         );
 
-        // Регистрация провайдера решений
         $this->app->make(SolutionProviderRepository::class)
             ->registerSolutionProvider(AISolutionProvider::class);
     }
